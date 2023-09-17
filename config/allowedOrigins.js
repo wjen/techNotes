6ff1a72c30501248438884194bfd7404f7bdd1e9
wen jen <wjen1214@gmail.com>
2023-09-17 11:23:36 -0700

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://technotes-4l6t.onrender.com.',
+    'http://technotes.cloudcatnine.com',
+];
 
 module.exports = allowedOrigins;
